23c1cdf46aabedf351357ee04df0465ac4ac647e
kubrayuce97 <kubrayuce97@gmail.com>
2022-01-22 10:16:13 +0300

    VSCode ile ilgili kısayollar kaydedildi.

---
+++ kısayollar.dart
@@ -0,0 +1,26 @@
+main(List<String> args) {
+
+// CTRL + X keser, CTRL + C kopyalar, CTRL + V yapıştırır.
+// CTRL + Z geri alır ,  CTRL +SHIFT + Z ileri sayar.
+// CTRL + C tüm satırı kopyalar.
+// CTRL + ok tuşları ile kelime ilerlemeyi sağlar.
+
+// CTRL + SHIFT + oklar ile kelime kelime seçmeyi sağlar.
+// ALT + yukarı/aşagı okları ile ilgili satır veya seçili alan taşınabilir.
+// ALT + click ile birden fazla imlece sahip olunabilir.
+// CTRL + K + C seçili alanı yorum satırı yapar.
+// CTRL + ö satırı yorum satırı yapar.
+// SHIFT + ALT + A seçili bloğu yorum satırı yapar 
+// SHIFT + ALT + yukarı/asagı okları satırı kopyalar.
+
+// CTRL + P dosya arar.
+// CTRL + F Dosyanın içinde arama yapar.
+// CTRL + ALT + F bul ve değiştir.
+// CTRL + SHIFT + F tüm dosyaları arar.
+
+// CTRL + B sol menuyu açar.
+// CTRL + tab bir sonraki sekme, CTRL + SHIFT + tab bir önceki sekme
+// CTRL + K + F seçili alanı, ALT + SHIFT + F tüm dosyayı formatlı gösterir.
+// CTRL + " terminali açar.
+
+}
